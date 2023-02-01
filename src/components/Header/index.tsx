128e4e9a33e56758c7eaa18739b3ca3332967d98
mariaclarabarbosa <clara_estrella@protonmail.com>
2023-02-01 18:58:05 -0300

    feat: add NewTransactionModal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
+import * as Dialog from "@radix-ui/react-dialog";
+
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import Logo from "../../assets/Logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={Logo} alt="" />
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
